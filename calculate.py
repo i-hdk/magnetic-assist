c1d86e2d7cb466a5ad90947c0b37484e301417bf
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2025-02-03 11:08:40 +0800

    update constants

--- calculate.py
+++ calculate.py
@@ -1,4 +1,7 @@
 #caps lock is constant
-PENDULUM_MASS = 16.53 #g
+PENDULUM_MASS = 16.53 #g, includes magnet + hanging mass
 STRING_LENGTH = 15 #cm
+HOVER_HEIGHT = 3 #cm, height of magnet above ground when at rest & no magnets underneath
 GRAVITY_ACCELERATION = 9.81 #m/s^2
+DIPOLE_MOMENT = 1.2 #Am^2 guessed
+MAGNET_SIZE = 0.9 #cm
