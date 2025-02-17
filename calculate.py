130ed4e326abe493148a40cbc177a09bf8e37b16
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2025-02-17 18:51:29 +0800

    add debug prints

--- calculate.py
+++ calculate.py
@@ -21,7 +21,7 @@ Z_LOWER_BOUND = 0
 Z_UPPER_BOUND = 25
 DEGREES_LOWER_BOUND = -90 #left (towards -x side) and 0 points to the bottom middle
 DEGREES_UPPER_BOUND = 90
-ITERATION_STEP = 1 #final should be around .1, big num for debug only
+ITERATION_STEP = 5 #final should be around .1, big num for debug only
 
 def degreeToCoordinate (deg)->tuple:
     x = math.sin(deg)*STRING_LENGTH
@@ -53,6 +53,8 @@ def calculateMagneticPotentialEnergy (x,y,z)->float:
 min_energy = 0
 first_iteration = True
 best_location = (0,0,0)
+best_gravitational_potential_energy = 0
+best_magnetic_potential_energy = 0
 
 #iterate deg bc assuming one directional pendulum        
 for deg in np.linspace(DEGREES_LOWER_BOUND,DEGREES_UPPER_BOUND, num = int((DEGREES_UPPER_BOUND-DEGREES_LOWER_BOUND)/ITERATION_STEP)+1):
@@ -64,15 +66,26 @@ for deg in np.linspace(DEGREES_LOWER_BOUND,DEGREES_UPPER_BOUND, num = int((DEGRE
     kinetic_energy = 0
     magnetic_potential_energy = calculateMagneticPotentialEnergy(magnet_x,magnet_y,magnet_z)
     energy = gravitational_potential_energy+kinetic_energy+magnetic_potential_energy
-    
+    print("gravity PE ",gravitational_potential_energy)
+    print("magnetic PE ",magnetic_potential_energy)
     if first_iteration:
         min_energy = energy
         best_location = (magnet_x,magnet_y,magnet_z)
         first_iteration = False
+        best_gravitational_potential_energy = gravitational_potential_energy
+        best_magnetic_potential_energy = magnetic_potential_energy
     if energy<min_energy:
         min_energy = energy
         best_location = (magnet_x,magnet_y,magnet_z)
+        best_gravitational_potential_energy = gravitational_potential_energy
+        best_magnetic_potential_energy = magnetic_potential_energy
         
+print("lowest energy location:")
 print(best_location)
+print("best: gravity PE ",best_gravitational_potential_energy)
+print("best: magnetic PE ",best_magnetic_potential_energy)
+
+
+
     
 
